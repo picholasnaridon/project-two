247219d35fa7120d4fe91c24500d76fba2707416
Andy Wilk <andrewdwilk@gmail.com>
2018-08-27 19:06:51 -0400

    error fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,9 +9,8 @@ module.exports = function(app) {
         //sent: false,   //for when we have this functionality implemented, for showing currently active messages list
         UserId: req.params.id
       }
-    
     }).then(function(messages) {
-      console.log(messages)
+      console.log(messages);
       res.json(messages);
     });
   });
@@ -28,7 +27,7 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/messages", function(req, res) {
-    console.log(req.body)
+    console.log(req.body);
     db.Message.create({
       body: req.body.body,
       sendTime: req.body.sendTime,
@@ -41,7 +40,7 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/messages/:id", function(req, res) {
-    db.Message.destroy({ where: { id: req.params.id } }).then( response => {
+    db.Message.destroy({ where: { id: req.params.id } }).then(response => {
       res.json(response);
     });
   });
