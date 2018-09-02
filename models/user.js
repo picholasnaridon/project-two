224b5307c0ab24c78141d9381c230a657f2a3288
Andy Wilk <andrewdwilk@gmail.com>
2018-09-02 16:05:17 -0400

    travis fix 6

--- models/user.js
+++ models/user.js
@@ -41,8 +41,8 @@ module.exports = function(sequelize, Sequelize) {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: "http://mainenordmenn.com/wp-content/uploads/2017/09/Maine-Nordmenn-Board-Generic-Profile.jpg"
+      
     }
-    
   });
 
   User.associate = function(models) {
